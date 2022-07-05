bd79ba13e9c21191e5f277adfdbd6c3b679df0b3
Joe <miskolczy.jozsef@pm.me>
2022-07-05 23:15:00 +0200

    SSR breaking tools removed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
+import cloudflare from '@astrojs/cloudflare';
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
+  site: "https://fullctx.dev",
+  adapter: cloudflare(),
   integrations: [tailwind()],
-  vite: {
-    ssr: {
-      external: ["@11ty/eleventy-img", "svgo"],
-    },
-  },
 });
